d4624d7bf4a038f8b63556fd0b5d29add5d53a71
Josh Larson <josh.larson@shopify.com>
2022-11-10 15:18:41 -0600

    Add a db:migrate:fresh NPM task

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,6 +6,7 @@ import { D1QB } from "workers-qb";
 
 type User = {
   id: number;
+  name: string;
   email: string;
   username: string;
 };
@@ -19,15 +20,32 @@ export async function loader({ context }: CloudflareLoaderArgs) {
   return json({ users: users.results });
 }
 
+/**
+ * Hash some text using Sha-256.
+ * NOTE: This is bad; don't do this; use bcrypt instead.
+ */
+async function hash(input: string) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hash));
+  return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+}
+
 export async function action({ request, context }: CloudflareActionArgs) {
   const formData = await request.formData();
+  const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const username = formData.get("username") as string;
+  const password = await hash(formData.get("password") as string);
 
   const qb = new D1QB(context.TWEETS_DB);
 
   try {
-    await qb.insert({ tableName: "users", data: { email, username } });
+    await qb.insert({
+      tableName: "users",
+      data: { email, username, name, password },
+    });
   } catch (error: any) {
     return json({ error: error.message }, { status: 400 });
   }
@@ -56,12 +74,20 @@ export default function Index() {
           <p style={{ color: "red" }}>{actionData.error}</p>
         )}
         <label>
+          Name
+          <input type="text" name="name" />
+        </label>
+        <label>
           Email
           <input type="email" name="email" />
         </label>
         <label>
           Username
           <input type="text" name="username" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
         </label>
         <button type="submit">Submit</button>
       </Form>
---
+++ db/migrations.sql
@@ -0,0 +1,10 @@
+drop table if exists users;
+create table users (
+  id serial primary key,
+  name text,
+  email text not null unique,
+  username text not null unique,
+  password text not null,
+  created_at timestamp not null default current_timestamp,
+  updated_at timestamp not null default current_timestamp
+);
