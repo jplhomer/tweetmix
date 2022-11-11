b8974f8942c9466d3074d03d003024f8a98ecaa3
Josh Larson <jplhomer@gmail.com>
2022-11-11 13:28:42 -0600

    Add Explore (all tweets)

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,9 +1,23 @@
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import type { TweetmixDataFunctionArgs } from "types";
 import { Heading } from "~/components/Text";
+import { TweetTimeline } from "~/components/TweetTimeline";
+import { Tweet } from "~/models/tweet.server";
+
+export async function loader({ context }: TweetmixDataFunctionArgs) {
+  return json({
+    tweets: await Tweet.all(context),
+  });
+}
 
 export default function Explore() {
+  const { tweets } = useLoaderData<typeof loader>();
+
   return (
     <div className="relative pt-16">
       <Heading>Explore</Heading>
+      <TweetTimeline tweets={tweets} />
     </div>
   );
 }
